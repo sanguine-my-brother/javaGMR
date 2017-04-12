3442528453589b8df7c6b82ef5ee5f75b8fb6c01
Danny ter Haar <dannyterhaar@msn.com>
2017-04-12 22:46:16 +0200

    feature: added icon

--- src/main/java/nl/hyranasoftware/javagmr/gui.java
+++ src/main/java/nl/hyranasoftware/javagmr/gui.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import nl.hyranasoftware.javagmr.views.fxml.JgmrGuiController;
@@ -24,21 +25,22 @@ import nl.hyranasoftware.javagmr.views.fxml.JgmrGuiController;
  * @author danny_000
  */
 public class gui extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
         FXMLLoader loader = null;
         String url = null;
-        url  = getClass().getResource("views/fxml/jgmrGui.fxml").toString();
-        System.out.println( "  * url: " + url );
+        url = getClass().getResource("views/fxml/jgmrGui.fxml").toString();
+        System.out.println("  * url: " + url);
         loader = new FXMLLoader(getClass().getResource("views/fxml/jgmrGui.fxml"));
         Parent root = null;
         try {
-            root = (Parent)loader.load();
+            root = (Parent) loader.load();
         } catch (IOException ex) {
             Logger.getLogger(gui.class.getName()).log(Level.SEVERE, null, ex);
         }
         Scene scene = new Scene(root);
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("eicon.png")));
         
         primaryStage.setTitle("Giant Multi Robot Java-Client");
         primaryStage.setScene(scene);
@@ -51,5 +53,5 @@ public class gui extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
